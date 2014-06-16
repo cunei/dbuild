822ff17156879e53842e7f7b708f0b8939c2dc39
Antonio Cunei <antonio.cunei@typesafe.com>
2014-06-17 01:33:35 +0200

    temp

--- distributed/repo/src/main/scala/distributed/repo/core/Repository.scala
+++ distributed/repo/src/main/scala/distributed/repo/core/Repository.scala
@@ -14,109 +14,75 @@ import project.model._
  * KeySource is the "uuid" supplier for put operations, which is not always
  * a DataType
  */
-sealed abstract class GetKey[DataType] extends {
+sealed protected class KeyUser {
+  def uuid: String
+  def key = path :+ uuid
+}
+sealed abstract class PutKey[DataType] extends KeyUser {
+  type Source <: { def uuid: String }
+  def source: Source
+  def uuid = source.uuid
+  def dataToStream(d: DataType): OutputStream
+}
+sealed abstract class GetKey[DataType] extends KeyUser {
   def uuid: String
   def streamToData(a: InputStream)(implicit m: Manifest[DataType]): DataType
 }
 sealed abstract class Key[DataType, Source <: { def uuid: String }, Get <: GetKey[DataType]] {
   def keyName: String
   def path: Seq[String]
-  def newGet(uuid:String): Get
-  def dataToStream(d: DataType): OutputStream
-/*
- * A "Key" also knows about how to build a selector from a key and a uuid,
- * and how to extract a uuid again from a selector.
- * A repository does not know anything about a selector, other than it is
- * a sequence of strings that represents a full key path under which
- * to store an unstructured stream of data.
- */
-  def selector(get:Get) = path :+ get.uuid
-  def uuid(selector:Seq[String]) = selector.tail
+}
+case class PutRaw(source: ArtifactSha) extends PutKey {
+  type Source = ArtifactSha
+  def dataToStream(d: Array[Byte]) = new java.io.ByteArrayOutputStream()
 }
 case class GetRaw(uuid: String) extends GetKey[Array[Byte]] {
-  def streamToData(a: InputStream)(implicit m: Manifest[Array[Byte]]) = Array[Byte]()
+  def streamToData(a: InputStream) = Array[Byte]()
 }
-abstract class GetMeta[DataType] extends GetKey[DataType] {
-  def streamToData(a: InputStream)(implicit m: Manifest[DataType]): DataType = /* gunzip */ sys.error("bah")
-}
-case class GetProject(uuid: String) extends GetMeta[RepeatableProjectBuild]
-case class GetBuild(uuid: String) extends GetMeta[SavedConfiguration]
-case class GetArtifacts(uuid: String) extends GetMeta[BuildArtifactsOut]
-case class GetExtract(uuid: String) extends GetMeta[ExtractionConfig]
 package object keys {
   implicit object RawKey extends Key[Array[Byte], ArtifactSha, GetRaw] {
     def keyName = "raw"
     def path = Seq(keyName)
-    def newGet(uuid: String) = GetRaw(uuid)
-    def dataToStream(d: Array[Byte]): OutputStream = new java.io.ByteArrayOutputStream()
-  }
-  // Data under any "Meta" key is compressed during serialization/deserialization,  directly while in transit.
-  private[keys] sealed abstract class KeyMeta[DataType, Source <: { def uuid: String }, Get <: GetKey[DataType]]
-    extends Key[DataType, Source, Get] {
-    def path = Seq("meta", keyName)
-    def dataToStream(d: DataType): OutputStream = new java.io.ByteArrayOutputStream()
-  }
-  implicit object ProjectKey extends KeyMeta[RepeatableProjectBuild, RepeatableProjectBuild, GetProject] {
-    def keyName = "project"
-    def newGet(uuid: String) = GetProject(uuid)
-  }
-  implicit object BuildKey extends KeyMeta[SavedConfiguration,SavedConfiguration, GetBuild] {
-    def keyName = "build"
-    def newGet(uuid: String) = GetBuild(uuid)
-  }
-  implicit object ArtifactsKey extends KeyMeta[BuildArtifactsOut,RepeatableProjectBuild, GetArtifacts] {
-    def keyName = "artifacts"
-    def newGet(uuid: String) = GetArtifacts(uuid)
-  }
-  implicit object ExtractKey extends KeyMeta[ExtractionConfig,ExtractionConfig, GetExtract] {
-    def keyName = "extract"
-    def newGet(uuid: String) = GetExtract(uuid)
   }
 }
 import keys._
 
 
-/**
- * Interface for a readable repository of data, indexed by key.
+sealed abstract class KeyMeta[DataType] extends Key[DataType] {
+  abstract case class PutMeta extends PutKey {
+    def dataToStream(d: DataType) = /* gzip */ new java.io.ByteArrayOutputStream()
+  }
+  abstract class GetMeta extends GetKey {
+    def streamToData(a: InputStream) = /* gunzip */ null
+  }
+  def path = Seq("meta", keyName)
+}
+
+object ProjectKey extends KeyMeta[RepeatableProjectBuild] {
+  case class PutProject(source: RepeatableProjectBuild) extends PutMeta {
+    type Source = RepeatableProjectBuild
+  }
+  case class GetProject(uuid: String) extends GetMeta
+  def keyName = "project"
+}
+
+/** Interface for a readable repository of data, indexed by key.
  *  All calls MUST be thread-safe, and safe for use by multiple processes.
  */
 abstract class ReadableRepository {
   /**
    * Retrieves the contents stored at a given key, if present.
    */
-  def get[DataType, Get <: GetKey[DataType]](g: Get)(implicit key: Key[DataType, _, Get], m: Manifest[DataType]): Option[DataType] = {
-    lock
-    val data = fetch(key.selector(g)) map { g.streamToData }
-    unlock
-    data
-  }
+  def get[DataType, Get](get: Get)(implicit key:Key[DataType,_,Get]): Option[DataType]
   /**
    * Retrieves the space concretely taken in the repository to store
    * the data indexed by this key. Returns zero if key not present.
    */
-  def getSize[DataType, Get <: GetKey[DataType]](g: Get)(implicit key: Key[DataType, _, Get], m: Manifest[Get]): Int = {
-    lock
-    val len = size(key.selector(g))
-    unlock
-    len
-  }
+  def getSize[DataType, Get](get: Get)(implicit key:Key[DataType,_,Get]): Int
   /**
    * Get the list of keys for items of this DataType currently in the repo
    */
-//  def enumerate[DataType, Get <: GetKey[DataType]](implicit key: Key[DataType, _, Get]): Seq[Get] = {
-//    lock
-//    val seq = scan(key.path) map { selector:Seq[String] => key.newGet(key.uuid(selector)) }
-//    unlock
-//    seq
-//  }
-
-  // the internal, non type safe versions, defined by the concrete implementations
-  protected def fetch(selector:Seq[String]): Option[InputStream]
-  protected def size(selector:Seq[String]): Int
-  // scan checks under the path "selector", and returns all the immediate subselectors containing data
-  protected def scan(selector:Seq[String]): Seq[String]
-  protected def lock:Unit
-  protected def unlock:Unit
+  def enumerate[DataType, Get](implicit key:Key[DataType,_,Get]): Seq[Get]
 }
 /** Abstract class representing the interface by which we can put/get data.
  *  All calls MUST be thread-safe, and safe for use by multiple processes.
@@ -124,30 +90,46 @@ abstract class ReadableRepository {
 abstract class WriteableRepository extends ReadableRepository {
   /** Puts some contents into the given key. */
   // wrong. KeySource is not parametrized, so it can be anything! Crap.
-  def put[DataType <: { def uuid: String }, Get <: GetKey[DataType]](data: DataType)(implicit key: Key[DataType, DataType, Get], m: Manifest[Get]): Get = put[DataType, DataType, Get](data, data)
-  def put[DataType, KeySource <: { def uuid: String }, Get <: GetKey[DataType]](data: DataType, keySource: KeySource)(implicit key: Key[DataType, KeySource, Get], m: Manifest[Get]): Get = {
-    lock
-    val uuid = keySource.uuid
-    val out = key.dataToStream(data)
-    val get = key.newGet(uuid)
-    store(out, key.selector(get))
-    unlock
-    get
+  def put[DataType, Get](data: DataType)(implicit key:Key[DataType,DataType,Get]): Get = put(data,data)
+  def put[DataType,KeySource, Get](data: DataType, keySource:KeySource)(implicit key:Key[DataType,KeySource,Get]): Get = {
+//    keySource.putKey
+    //
+    null
   }
-  // the internal, non type safe versions, defined by the concrete implementations
-  protected def store(out: OutputStream, selector: Seq[String]): Unit
 }
 
 object Test {
   def z(r:WriteableRepository, key1:RepeatableProjectBuild,key2:ArtifactSha, data: Array[Byte]) = {
-    val k1=r.put(key1)
-    val n=r.get(k1)
     val k2=r.put(data,key2)
     val m=r.get(k2)
+    val k1=r.put(key1)(ProjectKey)
+    val n=r.get(k1)
   }
 }
 
 
+
+// Data under the "Meta" key is compressed during serialization/deserialization,  directly while in transit.
+abstract sealed class GetMeta[T] extends GetKey[T] {
+  def dataToStream(d: T) = new java.io.ByteArrayOutputStream()
+  def streamToData(a: InputStream)(implicit m: Manifest[T]) = distributed.project.model.Utils.readValue[T]( /* add GUnZip to stream */ a)
+  protected def keyName: String
+  def key = "meta/" + keyName + "/" + uuid
+}
+case class KeyBuild(uuid: String) extends GetMeta[SavedConfiguration,SavedConfiguration] { def keyName = "build" }
+case class KeyArtifacts(uuid: String) extends GetMeta[BuildArtifactsOut,RepeatableProjectBuild] { def keyName = "artifacts" }
+case class KeyExtract(uuid: String) extends GetMeta[ExtractionConfig,ExtractionConfig] { def keyName = "extract" }
+//case class KeyProject(uuid: String) extends KeyMeta[RepeatableProjectBuild,RepeatableProjectBuild] { def keyName = "project" }
+//case class KeyBuild(uuid: String) extends KeyMeta[SavedConfiguration,SavedConfiguration] { def keyName = "build" }
+//case class KeyArtifacts(uuid: String) extends KeyMeta[BuildArtifactsOut,RepeatableProjectBuild] { def keyName = "artifacts" }
+//case class KeyExtract(uuid: String) extends KeyMeta[ExtractionConfig,ExtractionConfig] { def keyName = "extract" }
+
+// must add some sort of enumeration facility (we might need to abstract the generic path for each key type?)
+// Rather than exporting stuff for use by drepo, it makes instead sense to bring the enumeration/traversal
+// facilities needed by drepo INSIDE the Repository, which is where it should have lived all along.
+// Also, it would be nice to link once and for all each KeyMeta to the original source of UUIDs, so that we know
+// for instance that the uuid for a put for a BuildArtifactsOut *always* comes from a RepeatableProjectBuild.
+// So: a "put" could accept a further keySource of a related type.
 
 
 
