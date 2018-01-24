8edfeeeaf65312b22199e5c1fc96ab5c30f30cd5
Antonio Cunei <antonio.cunei@typesafe.com>
2018-01-24 14:55:56 +0100

    Add actor debugging while shutting down, or in case of timeouts

--- build/src/main/scala/com/typesafe/dbuild/build/LocalBuildMain.scala
+++ build/src/main/scala/com/typesafe/dbuild/build/LocalBuildMain.scala
@@ -54,16 +54,59 @@ class LocalBuildMain(repos: List[xsbti.Repository], options: BuildRunOptions) {
   def build(conf: DBuildConfiguration, confName: String, buildTarget: Option[String]): BuildOutcome = {
     implicit val timeout: Timeout = Timeouts.dbuildTimeout
     val result = builder ? RunLocalBuild(conf, confName, buildTarget)
-    Await.result(result.mapTo[BuildOutcome], Duration.Inf)
+    try {
+      Await.result(result.mapTo[BuildOutcome], Duration.Inf)
+    } catch {
+      case e: Exception => if (options.debug) {
+        println("Exception thrown while building!")
+        println("Actors after unexpected exception:\n" + dumpAllActors())
+        println("Exception caught:")
+        e.printStackTrace()
+        println("Rethrowing...")
+      }
+      throw e
+    }
   }
   def dispose(): Unit = {
     implicit val timeout: Timeout = 5.minutes
+    if (options.debug) {
+      println("Actors prior to logMgr exit:\n" + dumpAllActors())
+    }
     Await.result((logMgr ? "exit").mapTo[String], Duration.Inf)
+    if (options.debug) {
+      println("Actors after logMgr exit, prior to shutdown:\n" + dumpAllActors())
+    }
     system.shutdown() // pro forma, as all loggers should already be stopped at this point
     try {
       system.awaitTermination(4.minute)
     } catch {
-      case e:Exception => println("Warning: system did not shut down within the allotted time")
+      case e:Exception =>
+        println("Warning: system did not shut down within the allotted time")
+        if (options.debug) {
+          println(e.getMessage)
+          println("Actors still alive after awaitTermination() timeout:\n" + dumpAllActors())
+        }
     }
+  }
+
+  def dumpAllActors():String = {
+    class PrivateMethodCaller(x: AnyRef, methodName: String) {
+      def apply(_args: Any*): Any = {
+        val args = _args.map(_.asInstanceOf[AnyRef])
+        def _parents: Stream[Class[_]] = Stream(x.getClass) #::: _parents.map(_.getSuperclass)
+        val parents = _parents.takeWhile(_ != null).toList
+        val methods = parents.flatMap(_.getDeclaredMethods)
+        val method = methods.find(_.getName == methodName).getOrElse(throw new IllegalArgumentException("Method " + methodName + " not found"))
+            method.setAccessible(true)
+        method.invoke(x, args: _*)
+      }
+    }
+
+    class PrivateMethodExposer(x: AnyRef) {
+      def apply(method: scala.Symbol): PrivateMethodCaller = new PrivateMethodCaller(x, method.name)
+    }
+
+    val res = new PrivateMethodExposer(system)('printTree)()
+    res.toString
   }
 }
